f0cabe2ce568b15c63b94d0994e03333f55312c0
Ibrahim Awwal <ibrahima@hkn.EECS.Berkeley.EDU>
2010-03-21 22:46:40 -0700

    Changed backupdb.sh to store diffs for each daily backup instead of duplicating content, since apparently mainstream compression algorithms don't do deduplication that well.

--- backupdb.sh
+++ backupdb.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+# The HKN database backup script.
+# Written by ibrahima, sp10 compserv. See the git repo for commit history.
+# This script must be run as root, because root has automatic authorization
+# for postgres without having to enter the password.
+
 MMMYY=`date +"%b%y"`
+MONTH=`date +"%b"`
 DATE=`date +"%Y-%m-%d"`
 TIME=`date +"%H%M"`
 BACKUPHOME=/home/hkn/compserv/dbbackups
@@ -9,14 +16,22 @@ if [ ! -d $BACKUPPATH ]; then
     mkdir $BACKUPPATH
 fi
 cd $BACKUPPATH
-for DB in "${DBLIST[@]}"; do
+for DB in "${DBLIST[@]}"; do #Dump each database in the list
     MONTHLYARCH=$DB-$MMMYY
-    if [ -f $MONTHLYARCH.tar.bz2 ]; then
+    if [ -f $MONTHLYARCH.tar.bz2 ]; then #If we already made an archive of monthly backups, extract it.
 	tar -xjf $MONTHLYARCH.tar.bz2
     else
 	mkdir $MONTHLYARCH
     fi
-    pg_dump -U postgres $DB > $BACKUPPATH/$MONTHLYARCH/$DB-$DATE.sql
+    #Dump the current database into a file
+    pg_dump -U postgres $DB > $BACKUPPATH/$MONTHLYARCH/$DB-$DATE-$TIME.sql
+    if [ -f $BACKUPPATH/$MONTHLYARCH/$DB-$MONTH-base.sql ]; then
+	#If there is a base dump for the month, diff the current dump with the base and store that instead.
+	diff $BACKUPPATH/$MONTHLYARCH/$DB-$MONTH-base.sql $BACKUPPATH/$MONTHLYARCH/$DB-$DATE-$TIME.sql  >$BACKUPPATH/$MONTHLYARCH/$DB-$DATE-$TIME.diff
+	rm $BACKUPPATH/$MONTHLYARCH/$DB-$DATE-$TIME.sql
+    else
+	mv $BACKUPPATH/$MONTHLYARCH/$DB-$DATE-$TIME.sql $BACKUPPATH/$MONTHLYARCH/$DB-$MONTH-base.sql
+    fi
     tar -cjf $MONTHLYARCH.tar.bz2 $MONTHLYARCH/
     
     if [ -f $BACKUPHOME/$DB-latest.bz2 ]; then
