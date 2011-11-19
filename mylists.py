3bf6273632a61549225fa77a60bdd6fd871fec96
Ram Kandasamy <rkandasamy@hkn.EECS.Berkeley.EDU>
2011-11-19 15:51:59 -0800

    Script stubs for checking which lists you're on

---
+++ mylists.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+import os
+import mmlist
+
+def main():
+    print "Hello,", os.getlogin()
+
+if __name__ == "__main__":
+    main()
---
+++ mylists.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo -u namdoh /home/hkn/compserv/scripts/mmlist.py -b `whoami`
