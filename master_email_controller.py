e1c01bdc530951c799380408eb73c98e783d53c4
Ram Kandasamy <rkandasamy@hkn.EECS.Berkeley.EDU>
2012-03-14 01:14:43 -0700

    master_email_control now expands mailing lists too

--- master_email_controller.py
+++ master_email_controller.py
@@ -13,6 +13,7 @@ MMLIST_LOCATION = '/home/hkn/compserv/scripts/'
 MAILMAN_LOCATION = '/usr/lib/mailman/bin/'
 
 def parse_options():
+    global parser
     parser = OptionParser()
     parser.add_option('-l', '--list',  action='store_true', dest='list', default=False,
             help='list all mailing lists')
@@ -21,10 +22,52 @@ def parse_options():
             help='make -b act recursively')
 
     parser.add_option('-b', dest='expansion', metavar='expansion',
-            help='list mailing lists that the specified address (either an individual\'s email or another list) is on')
+            help='list mailing lists that the specified address (either an ' +
+            'individual\'s email or another list) is on')
+
+    parser.add_option('-e', dest='target', metavar='target',
+            help='list members of a mailing list')
 
     options, args = parser.parse_args()
     return (options, args)
+
+def main():
+    options, args = parse_options()
+    #print 'options:', options
+    #print 'args:', args
+
+    if options.list:
+        mmlist_lists = mmlist.list_lists()
+        mailman_lists = mailman.list_lists()
+        for mlist in mmlist_lists:
+            print mlist
+        for mlist in mailman_lists:
+            print mlist
+    elif options.expansion != None:
+        email = options.expansion
+        full_email = options.expansion
+        if '@' not in email:
+            full_email = email + '@hkn.eecs.berkeley.edu'
+
+        mmlist_lists = mmlist.find_member(email, options.recursive)
+        mailman_lists = mailman.find_member(full_email)
+
+        for mlist in mmlist_lists:
+            print mlist
+        for mlist in mailman_lists:
+            print mlist
+    elif options.target != None:
+        mmlist_members = mmlist.expand_list(options.target, options.recursive)
+        mailman_members = mailman.expand_list(options.target)
+        for addr in mmlist_members:
+            print addr
+        for addr in mailman_members:
+            print addr
+    else:
+        print parser.print_help()
+
+
+# Helper functions!
 
 def runproc(cmd, indata=None):
     proc = sp.Popen('python ' + cmd, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE, shell=True)
@@ -34,83 +77,76 @@ def runproc(cmd, indata=None):
     err = proc.stderr.read()
     return out, err
 
-def mmlist(*args):
-    return runproc(MMLIST_LOCATION + "mmlist.py " + " ".join(args))
+class mmlist:
+    @classmethod
+    def invoke(cls, *args):
+        result = runproc(MMLIST_LOCATION + "mmlist.py " + " ".join(args))
+        if result[1]:
+            raise Exception("mmlist error: " + result[1])
+        return result[0] 
 
-def mmlist_list_lists():
-    return mmlist("-l")
+    @classmethod
+    def list_lists(cls):
+        return [x for x in cls.invoke("-l").split("\n") if x]
 
-def mailman(*args, **keywords):
-    if "indata" in keywords.keys():
-        return runproc(MAILMAN_LOCATION + " ".join(args), indata = keywords["indata"])
-    else:
-        return runproc(MAILMAN_LOCATION + " ".join(args))
+    @classmethod
+    def add_member(cls, mlist, member):
+        return cls.invoke("-i", member, mlist)
 
-def mailman_list_lists():
-    return mailman("list_lists")
-
-def mailman_add_member(mlist, member):
-    return mailman("add_members", mlist, indata=member)
-
-def main():
-    options, args = parse_options()
-    #print 'options:', options
-    #print 'args:', args
-
-    if options.list:
-        mmlist_out, mmlist_err = mmlist_list_lists()
-        mailman_out, mailman_err = mailman_list_lists()
-
-        out = ''
-        if not mmlist_err and not mailman_err:
-            print mmlist_out, mailman_out
-        elif not mailman_err:
-            print 'Mailman Error:'
-            print mailman_err
-        elif not mmlist_err:
-            print 'mmlist Error:'
-            print mmlist_err
+    @classmethod
+    def find_member(cls, member, recursive):
+        if recursive:
+            result = cls.invoke("--no-error", "-rb", member)
         else:
-            print mmlist_err, mailman_err
-    elif options.expansion != None:
-        email = options.expansion
-        full_email = options.expansion
-        if '@' not in email:
-            full_email = email + '@hkn.eecs.berkeley.edu'
+            result = cls.invoke("--no-error", "-b", member)
+        return [x for x in result.split("\n") if x]
 
-        rflag = ""
-        if options.recursive:
-            rflag = "-r "
-
-        #print email, full_email
-        mmlist_proc = sp.Popen('python ' + MMLIST_LOCATION + 'mmlist.py ' + rflag + '-b ' + email, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE, shell=True)
-        mmlist_out = mmlist_proc.stdout.read()
-        mmlist_err = mmlist_proc.stderr.read()
-
-        mailman_proc = sp.Popen('python ' + MAILMAN_LOCATION + 'find_member ' + full_email, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE, shell=True)
-        mailman_out = mailman_proc.stdout
-        mailman_err = mailman_proc.stderr.read()
-
-        out = ''
-        if not mmlist_err and not mailman_err:
-            current_line = mailman_out.readline()
-            while current_line:
-                if full_email not in current_line:
-                    out += current_line.strip(' ')
-                current_line = mailman_out.readline()
-            print mmlist_out, out
-        elif not mailman_err:
-            print 'Mailman Error:'
-            print mailman_err
-        elif not mmlist_err:
-            print 'mmlist Error:'
-            print mmlist_err
+    @classmethod
+    def expand_list(cls, mlist, recursive):
+        if recursive:
+            result = cls.invoke("--no-error", "-re", mlist)
         else:
-            print mmlist_err, mailman_err
+            result = cls.invoke("--no-error", "-e", mlist)
+        return [x for x in result.split("\n") if x]
 
+class mailman:
+    @classmethod
+    def invoke(cls, *args, **keywords):
+        if "indata" in keywords.keys():
+            result = runproc(MAILMAN_LOCATION + " ".join(args), indata = keywords["indata"])
+        else:
+            result = runproc(MAILMAN_LOCATION + " ".join(args))
+        if result[1]:
+            raise Exception("Mailman error: " + result[1])
+        else:
+            return result[0] 
 
-    else:
-        print 'show options'
+    @classmethod
+    def list_lists(cls):
+        return [x for x in cls.invoke("list_lists", "-b").split("\n") if x]
+
+    @classmethod
+    def add_member(cls, mlist, member):
+        return cls.invoke("add_members", mlist, indata=member)
+
+    @classmethod
+    def find_member(cls, member):
+        result = [x.strip() for x in cls.invoke("find_member", "^"+member+"$").split("\n") if x]
+        if len(result) > 0:
+            return result[1:]
+        else:
+            return []
+
+    @classmethod
+    def expand_list(cls, mlist):
+        try:
+            result = cls.invoke("list_members", mlist)
+        except Exception as expt:
+            if "No such list:" in expt.args[0]:
+                return []
+            else:
+                raise Exception(expt.args[0])
+        return [x for x in result.split("\n") if x]
 
 
 if __name__ == "__main__":
