f17ed0e3cccea46a0c92c501b5c143c9224878b4
Ram Kandasamy <rkandasamy@hkn.EECS.Berkeley.EDU>
2012-03-15 17:21:18 -0700

    mailman_aliases outputs to stdout

--- mailman_aliases.sh
+++ mailman_aliases.sh
@@ -3,14 +3,13 @@
 # Using mailman's list_lists script, this will generate all the necessary
 # entries for the @@mailman file
 
-# $ ./mailman_aliases.sh ~hkn/compserv/mmlist/maillists/@@mailman
+# $ ./mailman_aliases.sh > ~hkn/compserv/mmlist/maillists/@@mailman
 
-echo > $1
 mm="|/var/lib/mailman/mail/mailman"
 for list in `/usr/lib/mailman/bin/list_lists -b`; do 
-    echo -e "$list:\t\"$mm post $list\"" >> $1
+    echo -e "$list:\t\"$mm post $list\""
     for action in admin bounces confirm join leave owner request subscribe unsubscribe; do
-        echo -e "$list-$action:\t\"$mm $action $list\"" >> $1
+        echo -e "$list-$action:\t\"$mm $action $list\""
     done
-    echo >> $1
+    echo
 done
