0bdb0b9ff88a5ef2e0b2d5dd784c105d75214398
root <root@hkn.EECS.Berkeley.EDU>
2014-05-31 09:26:10 -0700

    Removed flickr chmodding

--- flickr/flickr.sh
+++ flickr/flickr.sh
@@ -4,6 +4,4 @@
 # The script to backup Bridge's photos to Flickr.
 
 cd /home/hkn/compserv/scripts/flickr
-#chown -R hkn:bridge /hkn/bridge/flickr/pictures
-#chmod -R 775 /hkn/bridge/flickr/pictures
 ./FLICKRENV/bin/python uploader.py
